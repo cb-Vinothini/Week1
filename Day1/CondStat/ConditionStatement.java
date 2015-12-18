31d62d68f97cbdd9c4c21ed9c314fee4a2e4c41e
cbVinothini <vinothini@chargebee.com>
2015-12-18 17:01:52 +0530

    Condition Statement

---
+++ Day1/CondStat/ConditionStatement.java
@@ -0,0 +1,30 @@
+/**
+*conditionStatement class to implement the abstract working of if else statement
+*/
+public class ConditionStatement{
+  /**
+  *program starts execution here
+  */
+  public static void main(String[] args){
+    IfElse ifelse = new IfElse(); //object to invove ifElseWorking function
+    ifelse.ifElseWorking();
+  }
+}
+/**
+*IfEse class impelmented to explain the working of if else statement
+*/
+class IfElse{
+  /**
+  *demo working of if else statement
+  */
+  void ifElseWorking(){
+    if(true){
+      // if part is executed
+      System.out.println("If part");
+    }
+    else{
+      //else part is executed
+      System.out.println("Else part");
+    }
+  }
+}
