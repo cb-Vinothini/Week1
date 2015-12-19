ce0fa077c939fc8f3d11b137605b1cdb28f593e0
cbVinothini <vinothini@chargebee.com>
2015-12-19 16:55:54 +0530

    Fibo

---
+++ Day2/Fibonacci/FiboGeneration.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class FiboGeneration{
+  public static void main(String[] args){
+    System.out.print("Enter the number of terms: ");
+    Scanner scanner = new Scanner(System.in);
+    int count = scanner.nextInt();
+    int first_term = 0, second_term = 1, next_term;
+
+    System.out.print("First "+"count"+" terms of Fibonacci series : ");
+    for ( int index = 0 ; index < count ; index++ ){
+       if ( index <= 1 )
+          next_term = index;
+       else
+       {
+          next_term = first_term + second_term;
+          first_term = second_term;
+          second_term = next_term;
+       }
+       System.out.print(next_term+" ");
+    }
+    System.out.println();
+  }
+}
